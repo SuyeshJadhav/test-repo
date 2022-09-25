9391f12402c24e816ee805bd4a4866bfd5d05f91
SuyeshJadhav <suyesh.j@somaiya.edu>
2022-09-25 19:47:45 +0530

    main scanner

--- test.java
+++ test.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 class test{
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.console().reader());
         System.out.println("Hello");
+        int a = s.nextInt();
+        System.out.println("");
     }
 }
